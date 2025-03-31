470ee56e0a49842809ad869973167daaf96c36b5
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-31 19:48:03 -0300

    feat: configure Eloquent to create queries

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'mysql',
+    'database'  => 'database',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// eloquent
+require __DIR__ . '/../config/database.php';
+
 // includes routes files
 require_once __DIR__ . '/../src/routes/api.php';
 
