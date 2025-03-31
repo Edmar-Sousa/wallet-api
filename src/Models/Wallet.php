8f6b8f2712f1eaf66715647bd5f27412052a34f3
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-31 20:04:06 -0300

    feat: create wallet model

---
+++ src/Models/Wallet.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+
+    protected $table = 'wallet';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+
+    protected $fillable = [
+        'fullname',
+        'cpfCnpj',
+        'email',
+        'password',
+        'balance',
+    ];
+
+}
