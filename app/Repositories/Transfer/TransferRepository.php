ce4b05f7efb04fbc27e81fa83fafbe13fa7825a4
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-31 23:57:09 -0300

    feat: store transfer in database

---
+++ app/Repositories/Transfer/TransferRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories\Transfer;
+
+use App\Models\Transfer;
+
+class TransferRepository
+{
+
+    public function createTransfer(array $data)
+    {
+        $payer = $data['payer'];
+        $payee = $data['payee'];
+
+        return Transfer::create([
+            'payer' => $payer->id,
+            'payee' => $payee->id,
+            'value' => $data['value'],
+        ]);
+
+    }
+
+}
