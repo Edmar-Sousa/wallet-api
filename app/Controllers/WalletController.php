3a76c59e6abe72e6a8466aa0eccfd1de6eb496c8
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-31 22:39:16 -0300

    feat: method to create wallet user

--- app/Controllers/WalletController.php
+++ app/Controllers/WalletController.php
@@ -3,10 +3,25 @@
 namespace App\Controllers;
 
 use App\Models\Wallet;
+use App\Repositories\Wallet\WalletRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class WalletController
 {
+
+    public function createWallet(Request $request, Response $response): Response
+    {
+        $data = json_decode($request->getBody()->getContents(), true);
+
+        $walletRepository = new WalletRepository();
+        $wallet = $walletRepository->createUserWallet($data);
+
+        $response->getBody()
+            ->write(json_encode($wallet));
+
+        return $response->withStatus(201)
+            ->withHeader('Content-Type', 'application/json');
+    }
 
 }
