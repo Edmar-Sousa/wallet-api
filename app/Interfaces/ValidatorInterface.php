7199be2b41aa12a223a6e3dbd23e665ec663cc0e
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-01 23:57:04 -0300

    refact: remove function

--- app/Interfaces/ValidatorInterface.php
+++ app/Interfaces/ValidatorInterface.php
@@ -4,9 +4,5 @@ namespace App\Interfaces;
 
 interface ValidatorInterface
 {
-    public function isValid(): bool;
-    public function getErrorsMessage(): array;
     public function validate(array $data): void;
-    public function getErrorObject(): array;
-
 }
