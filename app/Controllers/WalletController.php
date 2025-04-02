9c44d0560a7e898193bcbce39d3e2d9157a0322f
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 00:45:29 -0300

    feat: method to find by wallet

--- app/Controllers/WalletController.php
+++ app/Controllers/WalletController.php
@@ -15,6 +15,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class WalletController
 {
+
+    public function findWallet(Request $request, Response $response, array $args): Response
+    {
+        try {
+            $walletUserCase = new UseCaseWallet();
+            $wallet = $walletUserCase->findWallet($args['id']);
+
+            $response->getBody()
+                ->write(json_encode($wallet));
+
+            return $response->withStatus(201)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+        catch (CustomException $err) {
+            $response->getBody()
+                ->write(json_encode($err->getErrorObject()));
+
+            return $response->withStatus($err->getCode())
+                ->withHeader('Content-Type', 'application/json');
+        }
+    }
+
     public function createWallet(Request $request, Response $response): Response
     {
         $walletValidator = ValidatorFactory::create(WalletType::USER);
