9ddb18854fb67cf7c13f81eee006f97556a30532
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 00:58:30 -0300

    refact: add constructor to simplify when create instance from class

--- app/Exceptions/PicPayAuthorizationException.php
+++ app/Exceptions/PicPayAuthorizationException.php
@@ -6,4 +6,14 @@ use App\Exceptions\CustomException;
 
 class PicPayAuthorizationException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'transaction_not_authorized',
+            403,
+            $messages
+        );
+    }
+
 }
