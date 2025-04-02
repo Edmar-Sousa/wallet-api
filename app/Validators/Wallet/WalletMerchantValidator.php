152d6f6714e19b32eac2609d62d2512761f5adb0
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-01 23:57:59 -0300

    refact: raise exception when validation fails

--- app/Validators/Wallet/WalletMerchantValidator.php
+++ app/Validators/Wallet/WalletMerchantValidator.php
@@ -2,37 +2,13 @@
 
 namespace App\Validators\Wallet;
 
+use App\Exceptions\ValidationException;
 use App\Interfaces\ValidatorInterface;
 use Respect\Validation\Exceptions\NestedValidationException;
 use Respect\Validation\Validator as v;
 
 class WalletMerchantValidator implements ValidatorInterface
 {
-    protected bool $isValid = false;
-
-    protected array $errorsMessage;
-
-
-    public function isValid(): bool
-    {
-        return $this->isValid;
-    }
-
-
-    public function getErrorsMessage(): array
-    {
-        return $this->errorsMessage;
-    }
-
-
-    public function getErrorObject(): array
-    {
-        return [
-            'status' => 400,
-            'code' => 'validation_wallet_error',
-            'errors' => $this->getErrorsMessage(),
-        ];
-    }
 
     public function validate(array $data): void
     {
@@ -43,16 +19,20 @@ class WalletMerchantValidator implements ValidatorInterface
                 ->key('cpfCnpj', v::cnpj()->notEmpty());
 
             $validator->assert($data);
-            $this->isValid = true;
         } catch (NestedValidationException $err) {
-            $this->errorsMessage = $err->getMessages([
+            $messages = $err->getMessages([
                 'string' => '{{name}} deve ser uma texto',
                 'empty' => '{{name}} não pode ser vazio',
                 'email' => '{{name}} deve ser um email valido',
                 'cnpj' => '{{name}} deve ser um cnpj valido',
             ]);
 
-            $this->isValid = false;
+            throw new ValidationException(
+                'Data to create wallet is invalid',
+                'validation_wallet_error',
+                400,
+                $messages
+            );
         }
     }
 }
