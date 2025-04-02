95d762ea7f147cd2047c227c034bb5ee4003cf7f
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:05:00 -0300

    refact: change create contructor

--- app/Exceptions/WalletBalanceInsufficientException.php
+++ app/Exceptions/WalletBalanceInsufficientException.php
@@ -8,4 +8,13 @@ use App\Exceptions\CustomException;
 
 class WalletBalanceInsufficientException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'wallet_balance_insufficient',
+            403,
+            $messages
+        );
+    }
 }
