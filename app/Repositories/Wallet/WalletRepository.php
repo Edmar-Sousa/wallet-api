a371304bcdecb317fadf16d08c57fa2aef330178
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-01 23:37:33 -0300

    feat: check if wallet exists before insert

--- app/Repositories/Wallet/WalletRepository.php
+++ app/Repositories/Wallet/WalletRepository.php
@@ -5,10 +5,23 @@ declare(strict_types=1);
 namespace App\Repositories\Wallet;
 
 use App\Enums\WalletType;
+use App\Exceptions\CreateWalletException;
 use App\Models\Wallet;
+
+use Exception;
 
 class WalletRepository
 {
+
+    private function hasWalletWithEmailOrCpfCnpj(string $email, string $cpfCnpj)
+    {
+        $wallet = Wallet::where('email', $email)
+            ->orWhere('cpfCnpj', $cpfCnpj)
+            ->first();
+
+        return $wallet === null;
+    }
+
     private function createWallet(array $data, WalletType $type, int $balance = 0): Wallet
     {
         return Wallet::create([
@@ -20,16 +33,34 @@ class WalletRepository
             // balance to tests
             'balance'  => $balance,
             'type'     => $type,
-        ]);
+        ]);        
     }
 
     public function createUserWallet(array $data): Wallet
     {
+        if ($this->hasWalletWithEmailOrCpfCnpj($data['email'], $data['cpfCnpj'])) {
+            throw new CreateWalletException(
+                'Error to create user wallet',
+                'error_create_wallet',
+                400,
+                [ 'message' => 'Ja existe uma carteira com o email ou cpf informado' ]
+            );
+        }
+
         return $this->createWallet($data, WalletType::USER, 1000000);
     }
 
     public function createMerchantWallet(array $data): Wallet
     {
+        if ($this->hasWalletWithEmailOrCpfCnpj($data['email'], $data['cpfCnpj'])) {
+            throw new CreateWalletException(
+                'Error to create merchant wallet',
+                'error_create_wallet',
+                400,
+                [ 'message' => 'Ja existe uma carteira com o email ou cnpj informado' ]
+            );
+        }
+
         return $this->createWallet($data, WalletType::MERCHANT, 1000000);
     }
 
