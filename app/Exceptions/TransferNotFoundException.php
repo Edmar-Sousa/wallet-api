c22bf5bb1af44ab03e0c89338101a72b702ffb8b
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 19:21:08 -0300

    feat: custom exception to transfer not found

---
+++ app/Exceptions/TransferNotFoundException.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use App\Exceptions\CustomException;
+
+class TransferNotFoundException extends CustomException
+{
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'transaction_not_found',
+            404,
+            $messages
+        );
+    }
+
+}
