7b68590ac054b9094a3b24008e60bbddfe1e1f66
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 00:56:02 -0300

    refact: default values

--- app/Exceptions/CreateWalletException.php
+++ app/Exceptions/CreateWalletException.php
@@ -6,4 +6,15 @@ namespace App\Exceptions;
 
 class CreateWalletException extends CustomException
 {
+
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'error_create_wallet',
+            400,
+            $messages
+        );
+    }
+
 }
