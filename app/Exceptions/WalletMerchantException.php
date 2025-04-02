89ab49575ce13ab4e4dd7c6748abd05348b945a3
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:06:46 -0300

    refact: create constructor

--- app/Exceptions/WalletMerchantException.php
+++ app/Exceptions/WalletMerchantException.php
@@ -8,4 +8,13 @@ use App\Exceptions\CustomException;
 
 class WalletMerchantException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'wallet_not_allowed_transfer',
+            403,
+            $messages
+        );
+    }
 }
