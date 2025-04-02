4cc017d7c120971c1012de8cc7b937c9ab033129
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 00:44:42 -0300

    feat: logic to find wallet

--- app/UseCases/UseCaseWallet.php
+++ app/UseCases/UseCaseWallet.php
@@ -5,11 +5,29 @@ declare(strict_types=1);
 namespace App\UseCases;
 
 use App\Enums\WalletType;
+use App\Exceptions\WalletNotFoundException;
 use App\Models\Wallet;
 use App\Repositories\Wallet\WalletRepository;
 
 class UseCaseWallet
 {
+    public function findWallet(int $walletId)
+    {
+        $walletRepository = new WalletRepository();
+        $wallet = $walletRepository->getWallet($walletId);
+
+        if ($wallet === null) {
+            throw new WalletNotFoundException(
+                'Error to find wallet with id ' . $walletId,
+                'wallet_not_found',
+                404,
+                ['wallet' => 'Carteira com id informado não foi encontrada']
+            );
+        }
+
+        return $wallet;
+    }
+
     public function createWallet(array $data, WalletType $type): Wallet
     {
         $walletRepository = new WalletRepository();
