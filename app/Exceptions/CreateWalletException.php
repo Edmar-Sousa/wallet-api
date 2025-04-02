5d31e90cf9d5cb1745cafbd59a968c6f8e340068
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:10:09 -0300

    style: run php cs fixer

--- app/Exceptions/CreateWalletException.php
+++ app/Exceptions/CreateWalletException.php
@@ -6,7 +6,6 @@ namespace App\Exceptions;
 
 class CreateWalletException extends CustomException
 {
-
     public function __construct(string $log, array $messages = [])
     {
         parent::__construct(
