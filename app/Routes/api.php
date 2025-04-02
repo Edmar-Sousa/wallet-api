5ee9255f1ff382236e21f963d464b218ca90908b
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-01 22:00:45 -0300

    feat: add route to transfer between wallets

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Controllers\TransferController;
 use App\Controllers\WalletController;
 
 
 $app->post('/wallet/user', [WalletController::class, 'createWallet']);
 $app->post('/wallet/merchant', [WalletController::class, 'createMerchantWallet']);
+$this->app->post('/transfer', [TransferController::class, 'createTransfer']);
