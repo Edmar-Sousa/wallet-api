cccdc2e814ae5043224a3d801f48aef030763062
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:09:22 -0300

    refact: create construct to simplify create new instances

--- app/Exceptions/WalletNotFoundException.php
+++ app/Exceptions/WalletNotFoundException.php
@@ -8,4 +8,13 @@ use App\Exceptions\CustomException;
 
 class WalletNotFoundException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'wallet_not_found',
+            404,
+            $messages
+        );
+    }
 }
