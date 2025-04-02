a0e28753fd5ad5edc40c9bc45756adce7a056def
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:02:51 -0300

    refact: create contruct to simplify create new instances

--- app/Exceptions/ValidationException.php
+++ app/Exceptions/ValidationException.php
@@ -6,4 +6,13 @@ use App\Exceptions\CustomException;
 
 class ValidationException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'validation_wallet_error',
+            400,
+            $messages
+        );
+    }
 }
