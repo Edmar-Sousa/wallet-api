684d1f4ab3b23cd6041015474337215f334de6ac
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-02 01:00:44 -0300

    refact: create construct to simplify instance of object

--- app/Exceptions/TransferException.php
+++ app/Exceptions/TransferException.php
@@ -6,4 +6,13 @@ use App\Exceptions\CustomException;
 
 class TransferException extends CustomException
 {
+    public function __construct(string $log, array $messages = [])
+    {
+        parent::__construct(
+            $log,
+            'transfer_failed',
+            403,
+            $messages
+        );
+    }
 }
