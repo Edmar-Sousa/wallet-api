1317fd2abaafd0bc11f99da7eafcb65b4cd74791
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-01 21:31:21 -0300

    feat: create a custom exception to handler erros

---
+++ app/Exceptions/CustomException.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use RuntimeException;
+
+class CustomException extends RuntimeException
+{
+    private string $errorCode;
+    private array $errorMessage;
+
+    final public function __construct(string $log, string $code, int $statusCode, array $messages = [])
+    {
+        parent::__construct($log, $statusCode);
+
+        $this->errorCode = $code;
+        $this->errorMessage = $messages;
+    }
+
+
+    final public function getErrorObject(): array
+    {
+        return [
+            'status' => $this->code,
+            'code' => $this->errorCode,
+            'errors' => $this->errorMessage,
+        ];
+    }
+}
