fd487d13469ab682748861c68028d6c61ef903d5
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-03 19:27:57 -0300

    fix: add middleware to handling errors

--- tests/Traits/BootApp.php
+++ tests/Traits/BootApp.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Traits;
 
+use App\Middlewares\ErrorHandlingMiddleware;
 use Slim\App;
 use Slim\Factory\AppFactory;
 
@@ -10,7 +11,12 @@ trait BootApp
     public function setUpApp(): App
     {
         require_once __DIR__ . '/../../config/database.php';
-        return AppFactory::create();
+
+        $app = AppFactory::create();
+
+        $app->add(ErrorHandlingMiddleware::class);
+
+        return $app;
     }
 
 }
