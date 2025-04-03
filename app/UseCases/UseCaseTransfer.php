29bb7b0240df9bf8f15cb6c24e28cfd2f5dfa3ba
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-03 08:39:22 -0300

    feat: convert valute to float before return

--- app/UseCases/UseCaseTransfer.php
+++ app/UseCases/UseCaseTransfer.php
@@ -136,6 +136,8 @@ class UseCaseTransfer
             $walletRepository->debtWallet($walletPayer, $amountTransfer);
             $walletRepository->creditWallet($walletPayee, $amountTransfer);
 
+            $transfer->value = floatval($transfer->value / 100);
+
             Capsule::commit();
             return $transfer;
         } catch (RuntimeException $e) {
