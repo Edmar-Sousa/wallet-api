1a3c149f25588b9bd5b1227506247bb6bbf60cee
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-04 20:43:23 -0300

    test: cancel transfer

--- tests/TestTransferController.php
+++ tests/TestTransferController.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use Slim\App;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Tests\Fixtures\TransferFixtures;
+use Tests\TestsFactory\TransferFactory;
 use Tests\TestsFactory\WalletFactory;
 use Tests\Traits\BootApp;
 
@@ -20,6 +21,28 @@ class TestTransferController extends TestCase
     {
         parent::setUp();
         $this->app = $this->setUpApp();
+    }
+
+    public function testCancelTransfer()
+    {
+        $balanceInCents = 10.50 * 100; // R$ 10.50 in cents
+
+        $userPayer = WalletFactory::createWalletInDatabaseWithoutBalance();
+        $userPayee = WalletFactory::createWalletInDatabaseWithoutBalance(balance: $balanceInCents);
+
+        $transfer = TransferFactory::createTransfer(
+            $userPayer->id,
+            $userPayee->id,
+            $balanceInCents
+        );
+
+        $request = (new ServerRequestFactory())->createServerRequest(
+            'DELETE',
+            "/transfers/{$transfer->id}/cancellation"
+        );
+
+        $response = $this->app->handle($request);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     public function testTryTransferInsufficientBalance()
