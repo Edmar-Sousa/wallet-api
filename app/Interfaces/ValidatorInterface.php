7f7dcf58de383f6ca783ebcd94605b17202259f5
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-06 00:11:31 -0300

    fix: add docs string to method

--- app/Interfaces/ValidatorInterface.php
+++ app/Interfaces/ValidatorInterface.php
@@ -4,5 +4,17 @@ namespace App\Interfaces;
 
 interface ValidatorInterface
 {
+    /**
+     * Apply rules to validate data to store wallet
+     * 
+     * @param array{
+     *  "fullname": string, 
+     *  "cpfCnpj": string, 
+     *  "email": string, 
+     *  "password": string 
+     * } $data
+     * 
+     * @return void
+     */
     public function validate(array $data): void;
 }
