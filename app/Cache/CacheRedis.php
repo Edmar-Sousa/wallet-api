32b8e6f00202dd03a7f2d82509f9ea6901ea801c
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-06 13:43:18 -0300

    feat: implement cache client to redis

---
+++ app/Cache/CacheRedis.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cache;
+
+use App\Interfaces\CacheInterface;
+
+
+class CacheRedis implements CacheInterface
+{
+    /**
+     * This function will place an event in the notification to 
+     * be executed
+     * 
+     * @return void
+     */
+    public function enqueueMessageToNotifier(string $channel, mixed $data): void
+    {
+        
+    }
+
+
+    /**
+     * This function will receive a notification event via a 
+     * queue and send a request to the external API.
+     * 
+     * It will keep trying until it receives a 
+     * successful response.
+     * 
+     * @return void
+     */
+    public function dequeueMessageToNotifier(string $channel): void
+    {
+
+    }
+}
